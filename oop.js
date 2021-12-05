886161137b0adb40d19c57648f77c86da1bf274f
AhmedSabry <a.sabryee@gmail.com>
2021-12-05 18:04:49 +0200

    Defining Object with new keyword

--- oop.js
+++ oop.js
@@ -1,50 +1,20 @@
 /*
     Defining Object
     [1] Object Literal
+    [2] with new keyword
 */
 
-let user = {
+let user = new Object();
 
-    // Properties
-    firstName: "Ahmed",
-    lastName: 'Sabry',
-    age: 28,
+user.firstName = "Ahmed";
+user.lastName = "Sabry";
+user["age"] = 28;
 
-    // Object
-    addresses: {
-        usa: "California",
-        eg: "Ismailia",
-        ksa: "Riyadh",
-
-        getMainAddress: () => `Main Address is in Egypt in city ${user.addresses.eg}`
-    },
-
-    // Methods
-    // Arrow function ES6
-    getFullName: () => `Full Name: ${user.firstName} ${user.lastName}`,
-    // Literal function 
-    getAgeInDays: function () {
-        return `Your Age in days ${user.age * 365}`;
-    } 
-    
+user.getFullName = function () {
+    return `Full name is ${user.firstName} ${user.lastName}`
 }
 
-// Accessing Object Properties
-console.log(user.firstName);  // Dot Notation
-console.log(user["lastName"]); // Bracket Notation
-
-console.log(typeof user.firstName);  // string
-
-
-// Accessing Object Methods
-console.log(typeof user.getFulName);  // function
-
+console.log(user.firstName);
+console.log(user["lastName"]);
+console.log(user.age);
 console.log(user.getFullName());
-console.log(user.getAgeInDays());
-
-
-// Accessing Object Nested Object
-console.log(user.addresses.eg);  // Dot Notation
-console.log(user["addresses"]["ksa"]);  // Bracket Notation
-
-console.log(user.addresses.getMainAddress());
