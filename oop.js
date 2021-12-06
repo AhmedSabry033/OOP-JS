9de7a8fd79ee4592287b2ece176065912550363b
AhmedSabry <a.sabryee@gmail.com>
2021-12-06 03:10:11 +0200

    Constructor Function

--- oop.js
+++ oop.js
@@ -1,53 +1,33 @@
 /*
-    Delete Operator
+    Constructor Function
 */
 
-const user = { name: "Ahmed"};
+// for Best Practice, it is better to write first letter of constructor name as capital
+function Phone (serial, color, price) {
+    this.serial = serial;
+    this.color = color;
+    this.price = price - 100;  // if i need to make a modification for one property from the main blueprint
+}
 
-console.log(user);
-console.log(user.name);
+let phone1 = new Phone(123, "Red", 500); // phone1 is instance of phone constructor
+let phone2 = new Phone(159, "Black", 500); // phone2 is instance of phone constructor
+let phone3 = Phone(167, "Silver", 500);  // phone3 is a variable at global scope, because we didn't identify it by "new" Keyword
 
-console.log(delete user);  // can't delete because it is const
-console.log(delete user.name);  // will return true because it is accepted to be deleted 
-// console.log(delete user["name"]); this is accepted syntax also
-
-console.log(user); // Now this object will be empty
-console.log(user.name);
-
-console.log("#".repeat(30));
-
-const username = "Ahmed";
-
-console.log(username);
-console.log(delete username);  // can't be deleted, so will return false
-console.log(username);
+console.log(phone1.serial);
+console.log(phone1.color);
+console.log(phone1.price);
+console.log(phone1 instanceof Phone);  // true
 
 console.log("#".repeat(30));
 
-const obj = Object.freeze( {prop: "value"} );  // No properties can be deleted or added to this object
-
-console.log(obj);
-console.log(obj.prop);
-console.log(delete obj.prop);  // can't be deleted because the object is freezed
-console.log(obj);
-console.log(obj.prop);
+console.log(phone2.serial);
+console.log(phone2.color);
+console.log(phone2.price);
+console.log(phone2 instanceof Phone);  // true
 
 console.log("#".repeat(30));
 
-const emptyObj = {};
-Object.defineProperty(emptyObj, "a", { value: 1, configurable: false });
-Object.defineProperty(emptyObj, "b", { value: 2, configurable: true });
-
-console.log(emptyObj);
-console.log(emptyObj.a);
-console.log(delete emptyObj.a);  // can't be deleted because configurable is false
-console.log(emptyObj);
-console.log(emptyObj.a);
-
-console.log("#".repeat(30));
-
-console.log(emptyObj);
-console.log(emptyObj.b);
-console.log(delete emptyObj.b);  // can't be deleted because configurable is true
-console.log(emptyObj);
-console.log(emptyObj.b);
+console.log(window.serial);
+console.log(window.color);
+console.log(window.price);
+console.log(phone3 instanceof Phone);  // false
