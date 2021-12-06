6f8feb1211f7a73343f68ba8bca18c69bffd32af
AhmedSabry <a.sabryee@gmail.com>
2021-12-06 03:27:31 +0200

    Constructor Function dealing with properties

--- oop.js
+++ oop.js
@@ -1,33 +1,30 @@
 /*
     Constructor Function
+    Dealing with properties
 */
 
-// for Best Practice, it is better to write first letter of constructor name as capital
-function Phone (serial, color, price) {
-    this.serial = serial;
-    this.color = color;
-    this.price = price - 100;  // if i need to make a modification for one property from the main blueprint
+function User (fName, lName, age, country) {
+    this.fName = fName;
+    this.lName = lName;
+    this.age = age;
+    this.country = country;
+
+    this.fullName = function() {
+        return `Full Name: ${this.fName} ${this.lName}`;
+    }
+
+    this.ageInDays = function() {
+        return `Age in Days: ${this.age * 365}`;
+    }
 }
 
-let phone1 = new Phone(123, "Red", 500); // phone1 is instance of phone constructor
-let phone2 = new Phone(159, "Black", 500); // phone2 is instance of phone constructor
-let phone3 = Phone(167, "Silver", 500);  // phone3 is a variable at global scope, because we didn't identify it by "new" Keyword
+let user1 = new User("Ahmed", "Sabry", 28, "Egypt");
+let user2 = new User("Ahmed", "Ali", 30, "Ksa");
 
-console.log(phone1.serial);
-console.log(phone1.color);
-console.log(phone1.price);
-console.log(phone1 instanceof Phone);  // true
+console.log(user1);
+console.log(user1.fullName());
+console.log(user1.ageInDays());
 
-console.log("#".repeat(30));
-
-console.log(phone2.serial);
-console.log(phone2.color);
-console.log(phone2.price);
-console.log(phone2 instanceof Phone);  // true
-
-console.log("#".repeat(30));
-
-console.log(window.serial);
-console.log(window.color);
-console.log(window.price);
-console.log(phone3 instanceof Phone);  // false
+console.log(user2);
+console.log(user2.fullName());
+console.log(user2.ageInDays());
